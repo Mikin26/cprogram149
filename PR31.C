22b84772b64dcd0f4c8843b2ded89bba9009e642
Mikin26 <mikinpatel.26@gmail.com>
2022-09-27 10:46:05 +0530

    c files

---
+++ PR31.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,n;
+clrscr();
+	printf("Enter the number:");
+	scanf("%d",&n);
+	for (a=1;a<=10;a++)
+	{
+	printf("%d*%d=%d\n",n,a,n*a);
+	}
+getch();
+}
